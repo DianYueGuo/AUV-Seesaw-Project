35c8318278a39d99e5eb1c483d466a4b20c3dc2c
DianYueGuo <dianyueguo@gmail.com>
2024-01-15 03:24:08 +0800

    remove build and devel files

--- catkin_ws_on_rpi/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws_on_rpi/src
-# Build directory: /root/catkin_ws_on_rpi/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("controller_pkg")
--- catkin_ws_on_rpi/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/root/catkin_ws_on_rpi/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/root/catkin_ws_on_rpi/devel;/opt/ros/noetic'.split(';') if '/root/catkin_ws_on_rpi/devel;/opt/ros/noetic' != '' else []
