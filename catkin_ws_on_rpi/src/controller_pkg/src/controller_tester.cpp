0105d8c3e8e48f7baf3ad8943f6d898db0cf799a
DianYueGuo <dianyueguo@gmail.com>
2024-01-15 06:08:45 +0800

    add controller tester

---
+++ catkin_ws_on_rpi/src/controller_pkg/src/controller_tester.cpp
@@ -0,0 +1,22 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "controller_tester");
+    ros::NodeHandle nh;
+
+    ros::Publisher pub = nh.advertise<std_msgs::String>("cpp_topic", 10);
+    ros::Rate rate(1);  // 1 Hz
+
+    while (ros::ok())
+    {
+        std_msgs::String msg;
+        msg.data = "Hello from C++!";
+        ROS_INFO("%s", msg.data.c_str());
+        pub.publish(msg);
+        rate.sleep();
+    }
+
+    return 0;
+}
