91617bb09c7cbe2a628b3f168c9b21dc1966efeb
DianYueGuo <dianyueguo@gmail.com>
2024-01-15 02:09:01 +0800

    wrap controller.cpp with catkin_ws_on_rpi

---
+++ catkin_ws_on_rpi/build/controller_pkg/CMakeFiles/controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/root/catkin_ws_on_rpi/devel/lib/controller_pkg/controller"
+  "/root/catkin_ws_on_rpi/devel/lib/controller_pkg/controller.pdb"
+  "CMakeFiles/controller.dir/src/controller.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws_on_rpi/build/controller_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/catkin_ws_on_rpi/src/controller_pkg
+# Build directory: /root/catkin_ws_on_rpi/build/controller_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
