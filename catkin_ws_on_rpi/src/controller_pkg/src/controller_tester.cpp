acbe85ebda088f73791911ef10c575e8254ec746
DianYueGuo <dianyueguo@gmail.com>
2024-01-16 20:01:41 +0800

    controller tester finished

--- catkin_ws_on_rpi/src/controller_pkg/src/controller_tester.cpp
+++ catkin_ws_on_rpi/src/controller_pkg/src/controller_tester.cpp
@@ -1,20 +1,63 @@
+#include <cmath>
+
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int64.h"
+
+const double position_limit = 100.0;
+
+const double acceleration = 5.0;
+
+double delta_pos = 2.0;
+double ball_velocity = 0.1;
+double delta_angle = 0.01;
+
+ros::Time previous_time;
+
+void update_physics()
+{
+    ros::Time current_time = ros::Time::now();
+    ros::Duration time_duration = current_time - previous_time;
+    previous_time = current_time;
+
+    delta_pos += ball_velocity * time_duration.toSec();
+    ball_velocity -= acceleration * time_duration.toSec() * sin(delta_angle);
+
+    if (delta_pos > position_limit)
+        delta_pos = position_limit;
+    if (delta_pos < -position_limit)
+        delta_pos = -position_limit;
+}
+
+void Callback(const std_msgs::Int64::ConstPtr &servo_angle_msg)
+{
+    delta_angle = (servo_angle_msg->data - 90) / 180.0 * M_PI;
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "controller_tester");
-    ros::NodeHandle nh;
 
-    ros::Publisher pub = nh.advertise<std_msgs::String>("cpp_topic", 10);
-    ros::Rate rate(1);  // 1 Hz
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("servo_angle_topic", 1000, Callback);
+    ros::Publisher pub = nh.advertise<std_msgs::Float32>("delta_pos_topic", 1000);
+
+    ros::Rate rate(60);
+
+    previous_time = ros::Time::now();
 
     while (ros::ok())
     {
-        std_msgs::String msg;
-        msg.data = "Hello from C++!";
-        ROS_INFO("%s", msg.data.c_str());
-        pub.publish(msg);
+        ROS_INFO("delta_angle: %.3f, delta_pos: %.3f, ball_velocity: %.3f", delta_angle, delta_pos, ball_velocity);
+
+        update_physics();
+
+        std_msgs::Float32 delta_pos_msg;
+        delta_pos_msg.data = delta_pos;
+        pub.publish(delta_pos_msg);
+
+        ros::spinOnce();
+
         rate.sleep();
     }
 
